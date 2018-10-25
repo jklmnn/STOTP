014e15a2c933cfffd668d545b5ed9d516e3d5ca1
Johannes Kliemann <jk@jkliemann.de>
2018-10-25 16:58:49 +0200

    add 2fa example

---
+++ examples/generate_2fa_token.adb
@@ -0,0 +1,36 @@
+with System;
+with Ada.Text_IO;
+with Ada.Command_Line;
+with LSC.Types;
+with LSC.Byte_Arrays;
+with OTP;
+with OTP.T;
+with Base32;
+use all type LSC.Types.Index;
+
+procedure Generate_2fa_Token
+  with SPARK_Mode
+is
+   function Time (Tloc : System.Address) return LSC.Types.Word64
+     with
+       Import,
+       Convention => C,
+       External_Name => "time";
+   package TOTP is new OTP.T;
+begin
+   pragma Assert (LSC.Byte_Arrays.Natural_Index'Last / 5 > 32);
+   pragma Warnings (Off, "no Global contract");
+   if Ada.Command_Line.Argument_Count = 1 and then
+     Ada.Command_Line.Argument (1)'Length >= 8 and then
+     Ada.Command_Line.Argument (1)'Length <= 40 and then
+     (Ada.Command_Line.Argument (1)'length mod 8 = 0 and
+     (for all C of Ada.Command_Line.Argument (1) =>
+          Base32.Valid_Base32_Character (C)))
+   then
+      Ada.Text_IO.Put_Line (OTP.Image (TOTP.TOTP (
+                            Base32.Decode (Ada.Command_Line.Argument (1)),
+                            Time (System.Null_Address)), 6));
+   else
+      Ada.Text_IO.Put_Line ("Usage: generate_2fa_token <base32 key (8, 16, 24, 32, 40 char)>");
+   end if;
+end Generate_2fa_Token;
